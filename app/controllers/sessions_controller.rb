e8be4d5da5a14f696a8ab77db49b2244be27cbea
jesontheg6 <jesonleejunzhen@gmail.com>
2018-03-01 18:57:03 +0800

    so far so good

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,61 @@
+class SessionsController < ApplicationController
+
+def new
+
+end
+
+
+  def create
+    user = User.where(email: params[:session][:email].downcase).first
+    if user && user.authenticate(params[:session][:password])
+       sign_in user
+      redirect_to "/users/feed"
+    else
+      flash[:error] = 'Invalid email/password combination'
+      redirect_to "/users/new"
+    end 
+    end 
+
+    def destroy
+    session.delete(:user_id)
+    @current_user = nil
+    flash[:notice] = "You've logged out"
+    redirect_to "/login"
+
+ end 
+
+ def confrim_email
+   user = User.where(email: params[:session][:email].downcase).first
+
+ end 
+
+
+  def create_from_omniauth
+      auth_hash = request.env["omniauth.auth"]
+      authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) ||  Authentication.create_with_omniauth(auth_hash)
+
+      # if: previously already logged in with OAuth
+      if authentication.user
+        user = authentication.user
+        authentication.update_token(auth_hash)
+        @next = "/users/#{user.id}/edit"
+        @notice = "Signed in!"
+      # else: user logs in with OAuth for the first time
+      else
+        user = User.create_with_auth_and_hash(authentication, auth_hash)
+        # you are expected to have a path that leads to a page for editing user details
+        @next = "/users/feed"
+        @notice = "User created. Please confirm or edit details"
+
+      end 
+
+      sign_in(user)
+      redirect_to @next, :notice => @notice
+    end
+
+
+  def show
+
+  end 
+
+end
